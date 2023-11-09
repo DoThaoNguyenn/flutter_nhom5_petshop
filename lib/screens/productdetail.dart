6504129c74f0f3c731a6c998c2839df6915c41d8
minhpttk <dinhminh060102@gmail.com>
2023-11-09 16:40:54 +0700

    product_detail&shipping_add

---
+++ lib/screens/productdetail.dart
@@ -0,0 +1,257 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../constants/image.dart';
+import '../model/item_cart_model.dart';
+import 'package:flutter/widgets.dart'; // Import gói flutter/widgets.dart
+import 'package:flutter/services.dart';
+class ProductDetailPage extends StatefulWidget {
+  @override
+  _ProductDetailPageState createState() => _ProductDetailPageState();
+}
+
+class _ProductDetailPageState extends State<ProductDetailPage> {
+  List<bool> starStates = [
+    false,
+    false,
+    false,
+    false,
+    false,
+  ]; // Initial state of stars
+  bool isFavorite = false; // Initial state of favorite button
+  int quantity = 1; // Initial quantity
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: Row(
+          children: [
+            IconButton(
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.red,
+              ),
+              onPressed: () {
+                // Handle back button press
+                Navigator.pop(context); // Quay trở lại trang trước đó
+              },
+            ),
+            SizedBox(width: 8),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'Product Name',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16),
+        children: [
+          SizedBox(height: 16),
+          Image.asset(img2
+            ,
+            height: 200,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: List.generate(
+                  5,
+                  (index) => IconButton(
+                    icon: Icon(
+                      Icons.star,
+                      color: starStates[index]
+                          ? Colors.orange
+                          : Colors.grey, // Star color based on state
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        // Update star states when a star is pressed
+                        for (int i = 0; i <= index; i++) {
+                          starStates[i] = true;
+                        }
+                        for (int i = index + 1; i < starStates.length; i++) {
+                          starStates[i] = false;
+                        }
+                      });
+                    },
+                  ),
+                ),
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.favorite,
+                  color: isFavorite
+                      ? Colors.red
+                      : Colors
+                          .black, // Change favorite button color based on state
+                ),
+                onPressed: () {
+                  setState(() {
+                    // Update favorite button state
+                    isFavorite = !isFavorite;
+                  });
+                },
+              ),
+            ],
+          ),
+          SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Product Name',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(
+                width: 120.0,
+                height: 50.0,
+                child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  RichText(
+                    textAlign: TextAlign.center,
+                    text: TextSpan(children: [
+                      TextSpan(
+                          text: '100.000',
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Montserrat',
+                              fontSize: 16,
+                              decoration: TextDecoration.lineThrough)),
+                      const TextSpan(text: '  '),
+                      TextSpan(
+                          text: '80.000',
+                          style: const TextStyle(
+                              color: Colors.black,
+                              fontFamily: 'Montserrat',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600))
+                    ]),
+                  ),
+                ],
+              ),
+             
+              )
+
+            ],
+          ),
+          SizedBox(height: 16),
+          Text(
+            'Product details',
+            style: TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(height: 16),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                  
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.red, // Set button background color
+                    onPrimary: Colors.white, // Set button text color
+                    
+                    padding: EdgeInsets.symmetric(
+                        vertical: 12, horizontal: 64), // Set button padding
+                    shape: RoundedRectangleBorder(
+                      borderRadius:
+                          BorderRadius.circular(25), // Set button border radius
+                    ),
+                  ),
+                  onPressed: () {
+                    addToCart(
+                        context); // Gọi hàm addToCart() khi nút được nhấp vào
+                  },
+                  child: Text('Add to Cart'),
+                ),
+
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      // Decrease quantity when decrease button is pressed
+                      if (quantity > 1) {
+                        quantity--;
+                      }
+                    });
+                  },
+                  child: Icon(Icons.remove),
+                ),
+                Text(
+                  quantity.toString(), // Display the quantity value
+                  style: TextStyle(fontSize: 16),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      // Increase quantity when increase button is pressed
+                      quantity++;
+                    });
+                  },
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(width: 16),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ProductDetailPage(),
+  ));
+}
+
+void addToCart(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext dialogContext) {
+      return AlertDialog(
+        content: Column(
+          mainAxisSize:
+              MainAxisSize.min, // Đảm bảo cột chỉ chiếm không gian cần thiết
+          children: [
+            Icon(
+              Icons.check_circle,
+              color: Colors.green,
+              size: 48,
+            ),
+            SizedBox(height: 8),
+            Text(
+              'Successful',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
---
+++ lib/screens/shipping_add.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+
+class ShippingAddressesPage extends StatefulWidget {
+  @override
+  _ShippingAddressesPageState createState() => _ShippingAddressesPageState();
+}
+
+class _ShippingAddressesPageState extends State<ShippingAddressesPage> {
+  String? selectedAddress;
+
+  void _updateSelectedAddress(String address) {
+    setState(() {
+      selectedAddress = address;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Shipping Addresses',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            ShippingAddressCard(
+              name: 'John Doe',
+              address: '123 Main St, City, State',
+              phoneNumber: '123-456-7890',
+              isSelected: selectedAddress == 'John Doe',
+              onSelect: _updateSelectedAddress,
+            ),
+            SizedBox(height: 16.0),
+            ShippingAddressCard(
+              name: 'Jane Smith',
+              address: '456 Elm St, City, State',
+              phoneNumber: '987-654-3210',
+              isSelected: selectedAddress == 'Jane Smith',
+              onSelect: _updateSelectedAddress,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // TODO: Xử lý hành động khi nhấn vào Floating Action Button
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+        elevation: 8.0,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+    );
+  }
+}
+
+class ShippingAddressCard extends StatelessWidget {
+  final String name;
+  final String address;
+  final String phoneNumber;
+  final bool isSelected;
+  final Function(String) onSelect;
+
+  const ShippingAddressCard({
+    required this.name,
+    required this.address,
+    required this.phoneNumber,
+    required this.isSelected,
+    required this.onSelect,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2.0,
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(phoneNumber),
+              ],
+            ),
+            SizedBox(height: 8.0),
+            Text(address),
+            SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Checkbox(
+                  value: isSelected,
+                  onChanged: (value) {
+                    onSelect(name);
+                  },
+                ),
+                Text('Use as the default shiping address'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        // TODO: Xử lý hành động chỉnh sửa địa chỉ
+                      },
+                      child: Text('Edit'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: ShippingAddressesPage(),
+  ));
+}
